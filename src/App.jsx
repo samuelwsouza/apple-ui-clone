9386e8216abef2e8cfee3c5de38d1cf0b21f9516
Samuel <samukawsouza@gmail.com>
2024-07-23 14:11:48 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
